0a6b83a412a4d477745226ade7cdd3868e5b643c
Michael Angerman <stormasm@gmail.com>
2017-09-25 12:07:51 -0700

    react-redux-d3 is now working under the create-react-app umbrella

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import ReactDOM from 'react-dom'
+import React from 'react'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Root from './Root'
+import configureStore from './store/configureStore'
+
+const store = configureStore(browserHistory)
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render(<Root store={store} history={history} />, document.getElementById('app'))

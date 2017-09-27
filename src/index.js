79432bcf4c1c85159cc6778f978b18f8f0119546
Michael Angerman <stormasm@gmail.com>
2017-09-27 10:24:27 -0700

    cc

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
-
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
-//import reducer from './reducers'
-//import App from './containers/App'
-//import Root from './Root'
 import rootReducer from './reducers/rootReducer'
-
 import Home from './components/containers/Home'
-//import DashboardOne from './components/containers/DashboardOne'
-//import LeftBar from './components/presentationals/LeftBar'
-//import Routes from './routes'
 
 const middleware = [ thunk ];
 if (process.env.NODE_ENV !== 'production') {
